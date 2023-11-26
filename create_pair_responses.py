b6d8dbdd551cb577b1928913df954aa0f5f7479b
Weixin Chen <48148205+chenweixin107@users.noreply.github.com>
2023-11-26 17:00:15 -0600

    Update create_pair_responses.py

--- create_pair_responses.py
+++ create_pair_responses.py
@@ -1,6 +1,4 @@
 import os
-os.environ['TRANSFORMERS_CACHE'] = '/data2/common/weixinchen/huggingface/cache/'
-
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional, Union
 import random
@@ -34,7 +32,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--data_name", type=str, default="ai2_arc")
 parser.add_argument("--subdata_name", type=str, default="ARC-Challenge")
 parser.add_argument("--split", type=str, default="train")
-parser.add_argument("--num_query", type=int, default=-1)
+parser.add_argument("--num_query", type=int, default=None)
 parser.add_argument("--tokenizer_name", type=str, default="meta-llama/Llama-2-7b-chat-hf")
 parser.add_argument("--model_name_or_path", type=str, default="meta-llama/Llama-2-7b-chat-hf")
 parser.add_argument('--useGT', action='store_true')
@@ -104,6 +102,51 @@ tqa_6shot_data = [
     },
 ]
 
+halu_6shot_data = [
+    {
+        "question": 'Which magazine was started first, Arthur\'s Magazine or First for Women?',
+        "candidate_correct": 'Arthur\'s Magazine.',
+        "candidate_incorrect": 'First for Women was started first.',
+        "correct": 'Arthur\'s Magazine.',
+        "incorrect": 'First for Women was started first.',
+    },
+    {
+        "question": 'The Oberoi family is part of a hotel company that has a head office in what city?',
+        "candidate_correct": 'Delhi.',
+        "candidate_incorrect": 'The Oberoi family\'s hotel company is based in Mumbai.',
+        "correct": 'Delhi.',
+        "incorrect": 'The Oberoi family\'s hotel company is based in Mumbai.',
+    },
+    {
+        "question": 'Musician and satirist Allie Goertz wrote a song about the "The Simpsons" character Milhouse, who Matt Groening named after who?',
+        "candidate_correct": 'President Richard Nixon.',
+        "candidate_incorrect": 'Allie Goertz wrote a song about Milhouse, a popular TV character, named after an influential political figure.',
+        "correct": 'President Richard Nixon.',
+        "incorrect": 'Allie Goertz wrote a song about Milhouse, a popular TV character, named after an influential political figure.',
+    },
+    {
+        "question": 'What nationality was James Henry Miller\'s wife?',
+        "candidate_correct": 'American.',
+        "candidate_incorrect": 'James Henry Miller\'s wife was British.',
+        "correct": 'American.',
+        "incorrect": 'James Henry Miller\'s wife was British.',
+    },
+    {
+        "question": 'Cadmium Chloride is slightly soluble in this chemical, it is also called what?',
+        "candidate_correct": 'Alcohol.',
+        "candidate_incorrect": 'Water with a hint of alcohol.',
+        "correct": 'Alcohol.',
+        "incorrect": 'Water with a hint of alcohol.',
+    },
+    {
+        "question": 'Which tennis player won more Grand Slam titles, Henri Leconte or Jonathan Stark?',
+        "candidate_correct": 'Jonathan Stark.',
+        "candidate_incorrect": 'Henri Leconte won more Grand Slam titles.',
+        "correct": 'Jonathan Stark.',
+        "incorrect": 'Henri Leconte won more Grand Slam titles.',
+    },
+]
+
 # Template
 if args.useGT:
     template_str = '<s>[INST] Consider the following question: {q}\nThe candidate correct answer is: {a_cand_correct}\nThe candidate in correct answer is: {a_cand_incorrect}\nPlease generate a correct answer and an incorrect answer. Make sure the answers are plausible. There is no need to give an explanation. [\INST]\nQuestion: {q}\nCorrect answer: {a_correct}\nIncorrect answer: {a_incorrect} </s>'
@@ -124,8 +167,8 @@ else:
 
 # Generate pair responses
 data_list = []
-for data_point in tqdm(train_dataset[:args.num_query]):
-    question, choices, answer_key = data_point["question"], data_point["choices"], data_point["answerKey"]
+num_query = len(train_dataset) if args.num_query == None else args.num_query
+for question, choices, answer_key in zip(train_dataset["question"][:num_query], train_dataset["choices"][:num_query], train_dataset["answerKey"][:num_query]):
     texts, labels = choices["text"], choices["label"]
 
     correct_idx = labels.index(answer_key)
@@ -175,7 +218,7 @@ for data_point in tqdm(train_dataset[:args.num_query]):
 print(f"There are {len(data_list)} training samples.")
 save_dir = "/data2/common/weixinchen/data/truthfulness"
 os.makedirs(save_dir, exist_ok=True)
-json_file = os.path.join(save_dir, f"{args.save_prefix}_num_{str(args.num_query)}_useGT_{str(args.useGT)}_useFS_{str(args.useFS)}.json")
+json_file = os.path.join(save_dir, f"{args.save_prefix}_num_{str(num_query)}_useGT_{str(args.useGT)}_useFS_{str(args.useFS)}.json")
 with open(json_file, "w") as file:
     for item in data_list:
         json.dump(item, file)
