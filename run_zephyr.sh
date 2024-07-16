a06386a63040cb0d6e3be50fe305d16a4bca3709
Weixin Chen <48148205+chenweixin107@users.noreply.github.com>
2024-07-15 23:23:07 -0700

    Update run_zephyr.sh

--- run_zephyr.sh
+++ run_zephyr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# DPO's reference model is set to be the current base model
+# DPO's reference model is set to the current base model
 run_python_command() {
    iteration=$1
    echo "Running iteration ${iteration}:"
@@ -32,41 +32,4 @@ run_python_command() {
 for iteration in {0..9..1}
 do
  run_python_command "$iteration"
-done
-
-
-# DPO's reference model is set to be the pretrained base model (recommended, which could avoid the overfitting problem)
-run_python_command() {
-    iteration=$1
-    seed=$2
-    echo "Running iteration ${iteration} with seed ${seed}:"
-
-    echo "Creating data..."
-    if [ "${iteration}" -eq 0 ]; then
-      python create_pair_responses.py --model_name_or_path="HuggingFaceH4/zephyr-7b-beta" --model_type="zephyr" --tokenizer_name="HuggingFaceH4/zephyr-7b-beta" --save_prefix="fix_ref_iter${iteration}_arcc" --useFS --seed=${seed}
-    else
-      python create_pair_responses.py --model_name_or_path="/data2/common/username/GRATH/models/zephyr/fix_ref_iter_${iteration}_arcc_num_1119_useGT_False_useFS_True_seed_${seed}/checkpoint-1000" --model_type="zephyr" --tokenizer_name="HuggingFaceH4/zephyr-7b-beta" --save_prefix="fix_ref_iter${iteration}_arcc" --useFS --seed=${seed}
-    fi
-
-
-    echo "Combining data..."
-    if [ "${iteration}" -eq 0 ]; then
-      echo "No need to combine data"
-    else
-      python combine_new_correct_ori_wrong.py --new_data_path="/data2/common/username/data/truthfulness/zephyr/fix_ref_iter${iteration}_arcc_num_1119_useGT_False_useFS_True_seed_${seed}.json" --ori_data_path="/data2/common/username/data/truthfulness/zephyr/iter0_arcc_num_1119_useGT_False_useFS_True_seed_${seed}.json"
-    fi
-
-    echo "Conducting DPO..."
-    if [ "${iteration}" -eq 0 ]; then
-      accelerate launch dpo_fix_ref.py --dataset_path="/data2/common/username/data/truthfulness/zephyr/fix_ref_iter${iteration}_arcc_num_1119_useGT_False_useFS_True_seed_${seed}.json" --model_name_or_path="HuggingFaceH4/zephyr-7b-beta" --model_type="zephyr" --tokenizer_name="HuggingFaceH4/zephyr-7b-beta" --output_dir="/data2/common/username/GRATH/models/zephyr/fix_ref_iter_$((iteration + 1))_arcc_num_1119_useGT_False_useFS_True" --seed=${seed}
-    else
-      accelerate launch dpo_fix_ref.py --dataset_path="/data2/common/username/data/truthfulness/zephyr/fix_ref_iter${iteration}_arcc_num_1119_useGT_False_useFS_True_seed_${seed}_combined.json" --model_name_or_path="/data2/common/username/GRATH/models/zephyr/fix_ref_iter_${iteration}_arcc_num_1119_useGT_False_useFS_True_seed_${seed}/checkpoint-1000" --model_type="zephyr" --tokenizer_name="HuggingFaceH4/zephyr-7b-beta" --output_dir="/data2/common/username/GRATH/models/zephyr/fix_ref_iter_$((iteration + 1))_arcc_num_1119_useGT_False_useFS_True" --seed=${seed}
-    fi
-}
-
-# Loop from 1 to 10 with a stride of 1
-for iteration in {0..9..1}
-do
-  seed=0
-  run_python_command "$iteration" "$seed"
 done
